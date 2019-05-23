ee420e421661dc38adb8bd8c95b072e17c3f9c53
INNOTEC\cesar_sanjose <cesar_sanjose@innotecsystem.com>
2019-05-23 12:04:48 +0200

    First and latest commit

---
+++ src/app/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { User } from './user/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  _user: User = {
+    name: "Bruce",
+    surname: "Willis"
+  }
+
+  getUser(){ 
+    return this._user; 
+  }
+
+  setUser(user){ 
+    this._user = user;
+  }
+
+}
